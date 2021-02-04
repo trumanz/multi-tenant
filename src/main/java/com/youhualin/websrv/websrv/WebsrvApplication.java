222d612b3e79220a2e70a7c1cf8ca386ea84931c
changkai.zhou <changkai.zhou@youhualin.com>
2021-02-04 15:29:48 +0800

    chg: initial codes from spring initializr

---
+++ src/main/java/com/youhualin/websrv/websrv/WebsrvApplication.java
@@ -0,0 +1,13 @@
+package com.youhualin.websrv.websrv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsrvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/youhualin/websrv/websrv/WebsrvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youhualin.websrv.websrv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsrvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
